422bcd8cf834b44ccfa0af86941628380608a570
Maximilian Popp <maxpopp90@yahoo.de>
2024-01-31 11:02:25 +0100

    burgermenu

--- js/board.js
+++ js/board.js
@@ -1,1 +1,4 @@
-
+function hideAddCardToBottomFromScreen() {
+    document.getElementById('addContactScreen').classList.add('hide-to-bottom-from-screen');
+    setTimeout(closeAddContactCard, 1000);
+}
