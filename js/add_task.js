c867c03b0603b7cc32a3f3d251a89736308331b1
Maximilian Popp <maxpopp90@yahoo.de>
2024-02-07 21:24:14 +0100

    task

--- js/add_task.js
+++ js/add_task.js
@@ -1,3 +1,6 @@
+/**
+ * Shows the Popup from the Side
+ */
 function showPopup() {
     console.log('Showing popup');
     document.getElementById('incomePopup').classList.remove('d-none');
@@ -8,4 +11,27 @@ function closePopup() {
     console.log('Closing popup');
     document.getElementById('incomePopup').classList.add('d-none');
     document.getElementById('incomePopup').classList.remove('income-popup');
+}
+
+/**
+ * save all Data to a Array for the Board-Card
+ */
+
+
+let informations = [];
+
+function addToInformations(){
+    let titel = document.getElementById('titel');
+    let description = document.getElementById('description');
+
+    let information = {
+        "titel": titel.value,
+        "description": description.value,
+    };
+
+    informations.push(information);
+    console.log(informations);
+    titel.value = '';
+    description.value = '';
+
 }
