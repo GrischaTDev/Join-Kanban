2fb0ab5ed109cf1721418c7485ba8336c939203c
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2024-02-14 18:19:47 -0300

    fixed the colors of priority buttons

--- js/add_task.js
+++ js/add_task.js
@@ -158,28 +158,31 @@ function togglePriority(priority) {
         // Wenn der aktuelle Button bereits ausgewählt ist, dann abwählen
         button.classList.remove('active');
         button.style.backgroundColor = ''; // Zurücksetzen der Hintergrundfarbe
-        button.style.color = '';
-        button.querySelector('img').style.filter = '';
+        button.style.color = ''; 
+        button.querySelector('img').style.filter = ''; 
     } else {
         // Andernfalls den aktuellen Button auswählen und den vorherigen abwählen
         var prevSelectedButton = document.querySelector('.priority-button.active');
         if (prevSelectedButton) {
             prevSelectedButton.classList.remove('active');
             prevSelectedButton.style.backgroundColor = ''; // Zurücksetzen der Hintergrundfarbe
-            prevSelectedButton.style.color = '';
-            prevSelectedButton.querySelector('img').style.filter = '';
+            prevSelectedButton.style.color = ''; 
+            prevSelectedButton.querySelector('img').style.filter = ''; 
         }
 
         button.classList.add('active');
         var computedStyle = getComputedStyle(button);
         button.style.backgroundColor = computedStyle.backgroundColor;
-        button.style.color = 'white';
-        button.querySelector('img').style.filter = 'brightness(0) invert(100%)';
+        button.style.color = 'white'; 
+        button.querySelector('img').style.filter = 'brightness(0) invert(100%)'; 
 
-        // Hintergrundfarbe für den Hover-Effekt setzen, wenn der Button ausgewählt ist
-        button.style.backgroundColor = '#ffa200';
+        // Hintergrundfarbe für den Medium-Button auf Gelb setzen
+        if (priority === 'medium') {
+            button.style.backgroundColor = '#ffa200';
+        }
     }
 }
+
 
 
 
