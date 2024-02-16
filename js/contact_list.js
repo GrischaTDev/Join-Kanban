9420eea4773d21c65e0222b7eb50cb2fbf87d404
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2024-02-16 10:28:43 -0300

    cleared Code of Contacts.js

---
+++ js/contact_list.js
@@ -0,0 +1,178 @@
+let contact_list = [
+    {
+        "id": "1",
+        "name": "Schulz",
+        "given_name": "Anja",
+        "e-mail": "schulz@hotmail.com",
+        "color": "rgb(255,122,0)",
+        "phone": "+49 1111 111 11 1"
+    },
+    {
+        "id": "2",
+        "name": "Mayer",
+        "given_name": "Anton",
+        "e-mail": "antom@gmail.com",
+        "color": "rgb(147,39,255)",
+        "phone": "+49 2222 222 22 2"
+    },
+    {
+        "id": "3",
+        "name": "Ziegler",
+        "given_name": "Benedict",
+        "e-mail": "benedict@gmail.com",
+        "color": "rgb(110,82,255)",
+        "phone": "+49 3333 333 33 3"
+    },
+    {
+        "id": "4",
+        "name": "Eisenberg",
+        "given_name": "David",
+        "e-mail": "davidberg@gmail.com",
+        "color": "rgb(252,113,255)",
+        "phone": "+49 4444 444 44 4"
+    },
+    {
+        "id": "5",
+        "name": "Mauer",
+        "given_name": "Emmanuel",
+        "e-mail": "emmanuelMa@gmail.com",
+        "color": "rgb(31,215,193)",
+        "phone": "+49 5555 555 55 5"
+    },
+    {
+        "id": "6",
+        "name": "Fischer",
+        "given_name": "Eva",
+        "e-mail": "eva@gmail.com",
+        "color": "rgb(255,187,43)",
+        "phone": "+49 6666 666 66 6"
+    },
+    {
+        "id": "7",
+        "name": "Bauer",
+        "given_name": "Marcel",
+        "e-mail": "bauer@gmail.com",
+        "color": "rgb(70,47,138)",
+        "phone": "+49 7777 777 77 7"
+    },
+    {
+        "id": "8",
+        "name": "Schmidt",
+        "given_name": "Timo",
+        "e-mail": "schmidtim@gmail.com",
+        "color": "rgb(128,128,186)",
+        "phone": "+49 4578 895 65 3"
+    },
+    {
+        "id": "9",
+        "name": "Peters",
+        "given_name": "Samuel",
+        "e-mail": "petersam@gmail.com",
+        "color": "rgb(247,247,1)",
+        "phone": "+49 7896 542 65 9"
+    },
+    {
+        "id": "10",
+        "name": "Becker",
+        "given_name": "Vincent",
+        "e-mail": "vibe@gmail.com",
+        "color": "rgb(235,74,143)",
+        "phone": "+49 7589 654 12 5"
+    },
+    {
+        "id": "11",
+        "name": "Braun",
+        "given_name": "Martin",
+        "e-mail": "mabra@gmail.com",
+        "color": "rgb(122,196,227)",
+        "phone": "+49 8527 419 63 2"
+    },
+    {
+        "id": "12",
+        "name": "Friesen",
+        "given_name": "Angelika",
+        "e-mail": "angi94@gmail.com",
+        "color": "rgb(2,109,186)",
+        "phone": "+49 3698 521 47 8"
+    },
+    {
+        "id": "13",
+        "name": "Lang",
+        "given_name": "Susanne",
+        "e-mail": "sula82@gmail.com",
+        "color": "rgb(237,27,38)",
+        "phone": "+49 1346 792 58 0"
+    },
+    {
+        "id": "14",
+        "name": "Klein",
+        "given_name": "Holger",
+        "e-mail": "hoklein@gmail.com",
+        "color": "rgb(46,161,108)",
+        "phone": "+49 9876 543 21 0"
+    },
+    {
+        "id": "15",
+        "name": "Giesbrecht",
+        "given_name": "Manfred",
+        "e-mail": "magies@gmail.com",
+        "color": "rgb(158,201,100)",
+        "phone": "+49 4598 984 98 5"
+    },
+    {
+        "id": "16",
+        "name": "Günther",
+        "given_name": "Lina",
+        "e-mail": "ligün@gmail.com",
+        "color": "rgb(240,142,30)",
+        "phone": "+49 1458 955 69 5"
+    },
+    {
+        "id": "17",
+        "name": "Albrecht",
+        "given_name": "Margret",
+        "e-mail": "marget@gmail.com",
+        "color": "rgb(131,87,166)",
+        "phone": "+49 1234 567 89 0"
+    },
+    {
+        "id": "18",
+        "name": "Kaiser",
+        "given_name": "Emil",
+        "e-mail": "emi@gmx.com",
+        "color": "rgb(42,54,71)",
+        "phone": "+49 1212 121 21 2"
+    },
+    {
+        "id": "19",
+        "name": "Regehr",
+        "given_name": "Paul",
+        "e-mail": "paureg@hotmail.com",
+        "color": "rgb(41,171,226)",
+        "phone": "+49 9999 999 99 9"
+    },
+    {
+        "id": "20",
+        "name": "Wolf",
+        "given_name": "Tatjana",
+        "e-mail": "wolf@gmail.com",
+        "color": "rgb(255,70,70)",
+        "phone": "+49 8888 888 88 8"
+    },
+    {
+        "id": "21",
+        "name": "Stork",
+        "given_name": "Marcel",
+        "e-mail": "bauer@gmail.com",
+        "color": "rgb(70,47,138)",
+        "phone": "+49 7777 777 77 7"
+    },
+    {
+        "id": "22",
+        "name": "Tänzer",
+        "given_name": "Tatjana",
+        "e-mail": "wolf@gmail.com",
+        "color": "rgb(255,70,70)",
+        "phone": "+49 8888 888 88 8"
+    }
+]
