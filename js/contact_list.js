32c2f5df497da02872c8dd78408662836c068bfd
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2024-03-10 23:14:44 -0300

    fixed contactslist

--- js/contact_list.js
+++ js/contact_list.js
@@ -8,7 +8,7 @@ let contact_list = [
         "given_name": "Anja",
         "e-mail": "schulz@hotmail.com",
         "color": "rgb(255,122,0)",
-        "phone": "+49 1111 111 11 1"
+        "phone": "015678965236"
     },
     {
         "id": "2",
@@ -16,7 +16,7 @@ let contact_list = [
         "given_name": "Anton",
         "e-mail": "antom@gmail.com",
         "color": "rgb(147,39,255)",
-        "phone": "+49 2222 222 22 2"
+        "phone": "02236985478"
     },
     {
         "id": "3",
@@ -24,7 +24,7 @@ let contact_list = [
         "given_name": "Benedict",
         "e-mail": "benedict@gmail.com",
         "color": "rgb(110,82,255)",
-        "phone": "+49 3333 333 33 3"
+        "phone": "021546978745"
     },
     {
         "id": "4",
@@ -32,7 +32,7 @@ let contact_list = [
         "given_name": "David",
         "e-mail": "davidberg@gmail.com",
         "color": "rgb(252,113,255)",
-        "phone": "+49 4444 444 44 4"
+        "phone": "0231456978742"
     },
     {
         "id": "5",
@@ -40,7 +40,7 @@ let contact_list = [
         "given_name": "Emmanuel",
         "e-mail": "emmanuelMa@gmail.com",
         "color": "rgb(31,215,193)",
-        "phone": "+49 5555 555 55 5"
+        "phone": "025896321458"
     },
     {
         "id": "6",
@@ -48,7 +48,7 @@ let contact_list = [
         "given_name": "Eva",
         "e-mail": "eva@gmail.com",
         "color": "rgb(255,187,43)",
-        "phone": "+49 6666 666 66 6"
+        "phone": "02136547899"
     },
     {
         "id": "7",
@@ -56,7 +56,7 @@ let contact_list = [
         "given_name": "Marcel",
         "e-mail": "bauer@gmail.com",
         "color": "rgb(70,47,138)",
-        "phone": "+49 7777 777 77 7"
+        "phone": "021456698745"
     },
     {
         "id": "8",
@@ -64,7 +64,7 @@ let contact_list = [
         "given_name": "Timo",
         "e-mail": "schmidtim@gmail.com",
         "color": "rgb(128,128,186)",
-        "phone": "+49 4578 895 65 3"
+        "phone": "056987412365"
     },
     {
         "id": "9",
@@ -72,7 +72,7 @@ let contact_list = [
         "given_name": "Samuel",
         "e-mail": "petersam@gmail.com",
         "color": "rgb(247,247,1)",
-        "phone": "+49 7896 542 65 9"
+        "phone": "0489652235654"
     },
     {
         "id": "10",
@@ -80,7 +80,7 @@ let contact_list = [
         "given_name": "Vincent",
         "e-mail": "vibe@gmail.com",
         "color": "rgb(235,74,143)",
-        "phone": "+49 7589 654 12 5"
+        "phone": "0698745566"
     },
     {
         "id": "11",
@@ -88,7 +88,7 @@ let contact_list = [
         "given_name": "Martin",
         "e-mail": "mabra@gmail.com",
         "color": "rgb(122,196,227)",
-        "phone": "+49 8527 419 63 2"
+        "phone": "04589654422"
     },
     {
         "id": "12",
@@ -96,7 +96,7 @@ let contact_list = [
         "given_name": "Angelika",
         "e-mail": "angi94@gmail.com",
         "color": "rgb(2,109,186)",
-        "phone": "+49 3698 521 47 8"
+        "phone": "0698745112"
     },
     {
         "id": "13",
@@ -104,7 +104,7 @@ let contact_list = [
         "given_name": "Susanne",
         "e-mail": "sula82@gmail.com",
         "color": "rgb(237,27,38)",
-        "phone": "+49 1346 792 58 0"
+        "phone": "02145896511"
     },
     {
         "id": "14",
@@ -112,7 +112,7 @@ let contact_list = [
         "given_name": "Holger",
         "e-mail": "hoklein@gmail.com",
         "color": "rgb(46,161,108)",
-        "phone": "+49 9876 543 21 0"
+        "phone": "078955621255"
     },
     {
         "id": "15",
@@ -120,15 +120,15 @@ let contact_list = [
         "given_name": "Manfred",
         "e-mail": "magies@gmail.com",
         "color": "rgb(158,201,100)",
-        "phone": "+49 4598 984 98 5"
+        "phone": "04598878956"
     },
     {
         "id": "16",
         "name": "Günther",
         "given_name": "Lina",
-        "e-mail": "ligün@gmail.com",
+        "e-mail": "lign@gmail.com",
         "color": "rgb(240,142,30)",
-        "phone": "+49 1458 955 69 5"
+        "phone": "01569787452"
     },
     {
         "id": "17",
@@ -136,7 +136,7 @@ let contact_list = [
         "given_name": "Margret",
         "e-mail": "marget@gmail.com",
         "color": "rgb(131,87,166)",
-        "phone": "+49 1234 567 89 0"
+        "phone": "0169857423323"
     },
     {
         "id": "18",
@@ -144,7 +144,7 @@ let contact_list = [
         "given_name": "Emil",
         "e-mail": "emi@gmx.com",
         "color": "rgb(42,54,71)",
-        "phone": "+49 1212 121 21 2"
+        "phone": "08654123565885"
     },
     {
         "id": "19",
@@ -152,7 +152,7 @@ let contact_list = [
         "given_name": "Paul",
         "e-mail": "paureg@hotmail.com",
         "color": "rgb(41,171,226)",
-        "phone": "+49 9999 999 99 9"
+        "phone": "016789654155"
     },
     {
         "id": "20",
@@ -160,7 +160,7 @@ let contact_list = [
         "given_name": "Tatjana",
         "e-mail": "wolf@gmail.com",
         "color": "rgb(255,70,70)",
-        "phone": "+49 8888 888 88 8"
+        "phone": "033654789955"
     },
     {
         "id": "21",
@@ -168,7 +168,7 @@ let contact_list = [
         "given_name": "Marcel",
         "e-mail": "bauer@gmail.com",
         "color": "rgb(70,47,138)",
-        "phone": "+49 7777 777 77 7"
+        "phone": "0578962124588"
     },
     {
         "id": "22",
@@ -176,6 +176,6 @@ let contact_list = [
         "given_name": "Tatjana",
         "e-mail": "wolf@gmail.com",
         "color": "rgb(255,70,70)",
-        "phone": "+49 8888 888 88 8"
+        "phone": "045484956151515"
     }
 ]
